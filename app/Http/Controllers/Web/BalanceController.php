18c29c5592e7e0b256cf41513cc382f9e5da503b
Ricardo-Zavala <ricardo.zavala@digitalsoft.mx>
2022-01-14 13:37:59 -0600

    notificacion por firebase

--- app/Http/Controllers/Web/BalanceController.php
+++ app/Http/Controllers/Web/BalanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Web\Deposit;
 use App\Http\Requests\BalanceRequest;
+use App\Repositories\Actions;
 use App\User;
 use Illuminate\Http\Request;
 use App\Web\Station;
@@ -33,13 +34,17 @@ class BalanceController extends Controller
     {
         $request->user()->authorizeRoles(['admin_master', 'admin_eucomb', 'admin_estacion']);
         $deposit->update(['status' => 2, 'deny' => null]);
-        if (($balance = Deposit::where([['client_id', $deposit->client->id], ['station_id', $deposit->station_id], ['status', 4]])->first()) != null) {
+        if ($balance = Deposit::where([['client_id', $deposit->client->id], ['station_id', $deposit->station_id], ['status', 4]])->first()) {
             $balance->balance += $deposit->balance;
             $balance->save();
         } else {
-            Deposit::create(['client_id' => $deposit->client->id, 'balance' => $deposit->balance, 'image_payment' => 'Saldo disponible para cliente', 'station_id' => $deposit->station_id, 'status' => 4]);
+            Deposit::create([
+                'client_id' => $deposit->client->id, 'balance' => $deposit->balance, 'image_payment' =>
+                'Saldo disponible para cliente', 'station_id' => $deposit->station_id, 'status' => 4
+            ]);
         }
-        $this->makeNotification($deposit->client->ids, 'Su abono ha sido aprobado');
+        $notification = new Actions();
+        $notification->sendNotification($deposit->client->ids, 'Su depósito ha sido aprobado');
         return redirect()->back()->withStatus(__('Abono autorizado'));
     }
     // Funcion para denegar el abono
@@ -48,9 +53,15 @@ class BalanceController extends Controller
         $request->user()->authorizeRoles(['admin_master', 'admin_eucomb', 'admin_estacion']);
         request()->validate(['deny' => 'required|string|min:3']);
         $deposit->update($request->merge(['status' => $estado ? 1 : 3])->only(['status', 'deny']));
-        $this->makeNotification($deposit->client->ids, $estado ?
-            'Su abono se colocó al estado pendiente' :
-            'Su abono ha sido denegado', $request->deny);
+        $notification = new Actions();
+        $notification->sendNotification(
+            $deposit->client->ids,
+            'Su depósito ha sido aprobado',
+            $estado ?
+                'Su abono se colocó al estado pendiente' :
+                'Su abono ha sido denegado',
+            $request->deny
+        );
         if ($estado) {
             if ($balance = Deposit::where([['client_id', $deposit->client->id], ['station_id', $deposit->station_id], ['status', 4]])->first()) {
                 $balance->balance -= $deposit->balance;
@@ -60,37 +71,6 @@ class BalanceController extends Controller
         return $estado ? redirect()->back()->withStatus('Abono actualizado al estado pendiente') :
             redirect()->back()->withStatus(__('Abono denegado.'));
     }
-    // Funcion para enviar un notificacion al cliente
-    private function makeNotification($ids, $status, $message = null)
-    {
-        $fields = array(
-            'app_id' => "62450fc4-bb2b-4f2e-a748-70e8300c6ddb",
-            'data' => array(
-                'message' => $message ? $message : '',
-            ),
-            'contents' => array(
-                "en" => "English message from postman",
-                "es" => $status
-            ),
-            'headings' => array(
-                "en" => "English title from postman",
-                "es" => "Abonos Eucomb"
-            ),
-            'include_player_ids' => [$ids],
-        );
-        $fields = json_encode($fields);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_exec($ch);
-        curl_close($ch);
-    }
-
     /**
      * Show the form for creating a new resource.
      *
